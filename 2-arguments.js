71e91442020f9b0428664093c38e6be46cb493fe
catherine mukima <mukimacatherine938@gmail.com>
2025-05-25 23:26:34 +0300

    add arguments to 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length ===2) {
+  console.log(' best Argument found');
+} else(args.length ==2)
+{
+  console.log('Best School Argument found');
+}
+
